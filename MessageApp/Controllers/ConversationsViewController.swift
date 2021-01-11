2e3556141a50dbb56b58e88e282169c7b3418ab7
Ibraczar <44230748+Ibraczar@users.noreply.github.com>
2021-01-11 19:34:17 +0300

    Create ConversationView

---
+++ MessageApp/Controllers/ConversationsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ConversationsViewController.swift
+//  MessageApp
+//
+//  Created by Ibragim Akaev on 11/01/2021.
+//
+
+import UIKit
+
+class ConversationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let isLoggedIn = UserDefaults.standard.bool(forKey: "Logged_in")
+        
+        if !isLoggedIn {
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen
+            present(nav, animated: false)
+        }
+    }
+
+
+}
+
